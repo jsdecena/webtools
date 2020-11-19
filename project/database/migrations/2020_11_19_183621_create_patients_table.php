9720694aee19dffa130b74b407d0528e96c1988e
Jeff <jeff.decena@yahoo.com>
2020-11-20 07:49:08 +1300

    Add the patients

---
+++ project/database/migrations/2020_11_19_183621_create_patients_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePatientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // http://www.hl7.org/fhir/patient.html
+        Schema::create('patients', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name');
+            $table->string('middle_name')->nullable();
+            $table->string('last_name');
+            $table->string('gender');
+            $table->boolean('active');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('patients');
+    }
+}
--- project/routes/web.php
+++ project/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\EmployeeController;
+use App\Http\Controllers\PatientController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -25,8 +27,9 @@ Route::get('login', function () {
 Route::post('login', [EmployeeController::class, 'authenticate'])->name('login');
 Route::get('logout', [EmployeeController::class, 'logout'])->name('logout');
 
-Route::get('dashboard', function () {
-    return view('dashboard');
-})->name('dashboard')->middleware('admin');
+Route::post('patients/{id}', [PatientController::class, 'update'])->name('patient.update');
+Route::get('patients/{id}/edit', [PatientController::class, 'edit'])->name('patient.edit');
+
+Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard')->middleware('admin');
 
 // Route::resource('employees', EmployeeController::class);
