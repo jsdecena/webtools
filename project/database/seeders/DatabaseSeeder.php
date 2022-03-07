faa7aee3a7e50157bea3691b58a14748584b9729
Jeff Simons Decena <jeff.decena@yahoo.com>
2022-03-07 14:14:55 +1300

    Update DatabaseSeeder.php

--- project/database/seeders/DatabaseSeeder.php
+++ project/database/seeders/DatabaseSeeder.php
@@ -14,6 +14,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
-        \App\Models\Employee::factory()->create();
+        \App\Models\Employee::factory(['email' => 'john@doe.com'])->create();
     }
 }
